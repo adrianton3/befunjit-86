0a221efe37d888083c7c3a31605f0680d1c91e99
Adrian Toncean <adrianton3@gmail.com>
2025-06-26 17:41:04 +0200

    add a benchmark that focuses on recompilations

--- bench/run.sh
+++ bench/run.sh
@@ -2,17 +2,22 @@
 
 cd "$(dirname "$0")"
 
+if [ ! -f "../test/spec/run-more/$1.bf" ]; then
+  echo "Could not find '../test/spec/run-more/$1.bf'"
+  exit 1
+fi
+
 echo "jit, with optimizations"
-sudo perf stat -r 100 ../cmake-build-release/b86 ../test/spec/run-more/mandelbrot.bf > /dev/null
+sudo perf stat -r 100 ../cmake-build-release/b86 ../test/spec/run-more/$1.bf > /dev/null
 
 echo "cooldown 2s"
 sleep 2
 
 echo "jit without optimizations"
-sudo perf stat -r 100 ../cmake-build-release/b86 --no-opt ../test/spec/run-more/mandelbrot.bf > /dev/null
+sudo perf stat -r 100 ../cmake-build-release/b86 --no-opt ../test/spec/run-more/$1.bf > /dev/null
 
 echo "cooldown 2s"
 sleep 2
 
 echo "interpreter"
-sudo perf stat -r 100 ./interpreter/bfi ../test/spec/run-more/mandelbrot.bf > /dev/null
+sudo perf stat -r 100 ./interpreter/bfi ../test/spec/run-more/$1.bf > /dev/null
