adrianton3/befunjit-86

commit dfd73232fa8f64218cce7843a66aac0907700170

optimize via more emplace_back and move

src/core/Graph.cpp
@@ -18,64 +18,70 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
         const auto pathlet = findPathlet(playfield, start);
 
         if (pathlet.loopbackIndex >= 0) {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, pathlet.loopbackIndex, 0 };
+            auto* path = new Path { std::move(pathlet.entries), pathlet.loopbackIndex };
             map.insert({ preStart, path });
             return path;
-        } else {
-            const PathletEntry& pathletEntryEnd = pathlet.entries.back();
+        }
 
-            if (pathletEntryEnd.value == '@') {
-                auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
-                map.insert({ preStart, path });
-                return path;
-            } else if (pathletEntryEnd.value == '_') {
-                auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
-                map.insert({ preStart, path });
+        const PathletEntry& pathletEntryEnd = pathlet.entries.back();
 
-                Cursor preStartRight = pathletEntryEnd.cursor;
-                preStartRight.directRight();
-                path->next0 = df(preStartRight);
+        if (pathletEntryEnd.value == '@') {
+            auto* path = new Path { std::move(pathlet.entries) };
+            map.insert({ preStart, path });
+            return path;
+        }
 
-                Cursor preStartLeft = pathletEntryEnd.cursor;
-                preStartLeft.directLeft();
-                path->next1 = df(preStartLeft);
+        if (pathletEntryEnd.value == '_') {
+            auto* path = new Path { std::move(pathlet.entries) };
+            map.insert({ preStart, path });
 
-                return path;
-            } else if (pathletEntryEnd.value == '|') {
-                auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
-                map.insert({ preStart, path });
+            Cursor preStartRight = pathletEntryEnd.cursor;
+            preStartRight.directRight();
+            path->next0 = df(preStartRight);
 
-                Cursor preStartDown = pathletEntryEnd.cursor;
-                preStartDown.directDown();
-                path->next0 = df(preStartDown);
+            Cursor preStartLeft = pathletEntryEnd.cursor;
+            preStartLeft.directLeft();
+            path->next1 = df(preStartLeft);
 
-                Cursor preStartUp = pathletEntryEnd.cursor;
-                preStartUp.directUp();
-                path->next1 = df(preStartUp);
+            return path;
+        }
 
-                return path;
-            } else if (pathletEntryEnd.value == '?') {
-                auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
-                map.insert({ preStart, path });
+        if (pathletEntryEnd.value == '|') {
+            auto* path = new Path { std::move(pathlet.entries) };
+            map.insert({ preStart, path });
 
-                Cursor preStartUp = pathletEntryEnd.cursor;
-                preStartUp.directUp();
-                path->next0 = df(preStartUp);
+            Cursor preStartDown = pathletEntryEnd.cursor;
+            preStartDown.directDown();
+            path->next0 = df(preStartDown);
 
-                Cursor preStartLeft = pathletEntryEnd.cursor;
-                preStartLeft.directLeft();
-                path->next1 = df(preStartLeft);
+            Cursor preStartUp = pathletEntryEnd.cursor;
+            preStartUp.directUp();
+            path->next1 = df(preStartUp);
 
-                Cursor preStartDown = pathletEntryEnd.cursor;
-                preStartDown.directDown();
-                path->next2 = df(preStartDown);
+            return path;
+        }
 
-                Cursor preStartRight = pathletEntryEnd.cursor;
-                preStartRight.directRight();
-                path->next3 = df(preStartRight);
+        if (pathletEntryEnd.value == '?') {
+            auto* path = new Path { std::move(pathlet.entries) };
+            map.insert({ preStart, path });
 
-                return path;
-            }
+            Cursor preStartUp = pathletEntryEnd.cursor;
+            preStartUp.directUp();
+            path->next0 = df(preStartUp);
+
+            Cursor preStartLeft = pathletEntryEnd.cursor;
+            preStartLeft.directLeft();
+            path->next1 = df(preStartLeft);
+
+            Cursor preStartDown = pathletEntryEnd.cursor;
+            preStartDown.directDown();
+            path->next2 = df(preStartDown);
+
+            Cursor preStartRight = pathletEntryEnd.cursor;
+            preStartRight.directRight();
+            path->next3 = df(preStartRight);
+
+            return path;
         }
 
         return nullptr;
@@ -83,7 +89,7 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
 
     auto* start = df(cursor);
 
-    return Graph { map, start };
+    return Graph { std::move(map), start };
 }
 
 std::string stringify (const Graph& graph) {

src/core/Graph.h
@@ -7,6 +7,11 @@
 
 
 struct Graph {
+    Graph (std::unordered_map<Cursor, Path*>&& _map, Path* _start)
+    : map { std::move(_map) }
+    , start { _start }
+    {}
+
     std::unordered_map<Cursor, Path*> map;
     Path* start;
 

src/core/Path.h
@@ -9,6 +9,16 @@
 struct Path;
 
 struct Path {
+    Path (const std::vector<PathletEntry>&& _entries, int16_t _loopbackIndex = 0)
+    : entries { std::move(_entries) }
+    , next0 { nullptr }
+    , next1 { nullptr }
+    , next2 { nullptr }
+    , next3 { nullptr }
+    , loopbackIndex { _loopbackIndex }
+    , startIndex { 0 }
+    {}
+
     std::vector<PathletEntry> entries;
     Path* next0;
     Path* next1;

src/parts/runLine.cpp
@@ -84,7 +84,7 @@ void part::runLine (const std::string& fileName) {
         });
     }
 
-    Path path { pathletEntries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
+    Path path { std::move(pathletEntries) };
 
     int64_t stash[16];
 

src/core/Pathlet.cpp
@@ -24,7 +24,7 @@ Pathlet findPathlet (const Playfield& playfield, Cursor cursor) {
                 index++;
             }
 
-            entries.push_back({ cursor, value, stringMode });
+            entries.emplace_back(cursor, value, stringMode);
             cursor.advance();
 
             continue;
@@ -41,15 +41,15 @@ Pathlet findPathlet (const Playfield& playfield, Cursor cursor) {
         }
 
         if (const auto indexMaybe = map.find(cursor); indexMaybe != map.end()) {
-            return { entries, indexMaybe->second };
+            return { std::move(entries), indexMaybe->second };
         }
 
         map.insert({ cursor, index });
 
-        entries.push_back({ cursor, value, false });
+        entries.emplace_back(cursor, value, false);
 
         if (value == '_' || value == '|' || value == '?' || value == '@') {
-            return { entries, -1 };
+            return { std::move(entries), -1 };
         }
 
         if (value == '#') {
@@ -64,7 +64,7 @@ Pathlet findPathlet (const Playfield& playfield, Cursor cursor) {
         index++;
     }
 
-    return { entries, -1 };
+    return { std::move(entries), -1 };
 };
 
 std::string stringify (const PathletEntry& entry) {

src/core/push.cpp
@@ -179,8 +179,8 @@ void push::mul1 (std::vector<uint8_t>& bytes, int64_t value) {
     // but this bit of code doesn't know that
     bytes.insert(bytes.end(), {
         0x48, 0xb8, getByte<0>(value), getByte<1>(value), getByte<2>(value), getByte<3>(value), getByte<4>(value), getByte<5>(value), getByte<6>(value), getByte<7>(value),  // mov rax, 64bit
-        0x48, 0x0f, 0xaf, 0x44, 0xf7, 0xf8,    // imul rax, [rdi + rsi * 8 - 8]
-        0x48, 0x89, 0x44, 0xf7, 0xf8,          // mov [rdi + rsi * 8 - 8], rax
+        0x48, 0x0f, 0xaf, 0x44, 0xf7, 0xf8,        // imul rax, [rdi + rsi * 8 - 8]
+        0x48, 0x89, 0x44, 0xf7, 0xf8,              // mov [rdi + rsi * 8 - 8], rax
     });
 }
 
@@ -399,9 +399,9 @@ void push::put2NoRecomp (std::vector<uint8_t>& bytes, int64_t const* playfieldDa
 
     bytes.insert(bytes.end(), {
         0x48, 0xb8, getByte<0>(playfieldDataAddress), getByte<1>(playfieldDataAddress), getByte<2>(playfieldDataAddress), getByte<3>(playfieldDataAddress), getByte<4>(playfieldDataAddress), getByte<5>(playfieldDataAddress), getByte<6>(playfieldDataAddress), getByte<7>(playfieldDataAddress), // mov rax, playfieldDataAddress
-        0x48, 0x8b, 0x54, 0xf7, 0xf8, // mov rdx, [rdi + rsi * 8 - 8]
+        0x48, 0x8b, 0x54, 0xf7, 0xf8,              // mov rdx, [rdi + rsi * 8 - 8]
         0x48, 0x89, 0x90, getByte<0>(offsetFlat), getByte<1>(offsetFlat), getByte<2>(offsetFlat), getByte<3>(offsetFlat), // mov [rax + offsetFlat], rdx
-        0x48, 0xff, 0xce, // dec rsi
+        0x48, 0xff, 0xce,                          // dec rsi
     });
 }
 
@@ -519,7 +519,7 @@ void push::loopback (std::vector<uint8_t>& bytes, int64_t loopbackIndex) {
     // if delta fits in 8bits then use the shorter jump
 
     bytes.insert(bytes.end(), {
-        0xE9, getByte<0>(delta), getByte<1>(delta), getByte<2>(delta), getByte<3>(delta)               // jmp with 32bit offset
+        0xE9, getByte<0>(delta), getByte<1>(delta), getByte<2>(delta), getByte<3>(delta) // jmp with 32bit offset
     });
 }
 

commit 1e2e8a4089671bc8dd7c2b48de1b3d1b2d89cc3a

fix the bug introduced in dfd7323
it had nothing to do with move
the optimizations were just turned off

src/core/Graph.cpp
@@ -15,10 +15,10 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
 
         Cursor start = preStart;
         start.advance();
-        const auto pathlet = findPathlet(playfield, start);
+        auto pathlet = findPathlet(playfield, start);
 
         if (pathlet.loopbackIndex >= 0) {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, pathlet.loopbackIndex, 0 };
+            auto* path = new Path { std::move(pathlet.entries), pathlet.loopbackIndex };
             map.insert({ preStart, path });
             return path;
         }
@@ -26,13 +26,13 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
         const PathletEntry& pathletEntryEnd = pathlet.entries.back();
 
         if (pathletEntryEnd.value == '@') {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
+            auto* path = new Path { std::move(pathlet.entries) };
             map.insert({ preStart, path });
             return path;
         }
 
         if (pathletEntryEnd.value == '_') {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
+            auto* path = new Path { std::move(pathlet.entries) };
             map.insert({ preStart, path });
 
             Cursor preStartRight = pathletEntryEnd.cursor;
@@ -47,7 +47,7 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
         }
 
         if (pathletEntryEnd.value == '|') {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
+            auto* path = new Path { std::move(pathlet.entries) };
             map.insert({ preStart, path });
 
             Cursor preStartDown = pathletEntryEnd.cursor;
@@ -62,7 +62,7 @@ Graph findGraph (const Playfield& playfield, const Cursor& cursor) {
         }
 
         if (pathletEntryEnd.value == '?') {
-            auto* path = new Path { pathlet.entries, nullptr, nullptr, nullptr, nullptr, -1, 0 };
+            auto* path = new Path { std::move(pathlet.entries) };
             map.insert({ preStart, path });
 
             Cursor preStartUp = pathletEntryEnd.cursor;

src/core/Graph.h
@@ -7,7 +7,7 @@
 
 
 struct Graph {
-    Graph (std::unordered_map<Cursor, Path*>&& _map, Path* _start)
+    Graph (std::unordered_map<Cursor, Path*> _map, Path* _start)
     : map { std::move(_map) }
     , start { _start }
     {}

src/core/Path.h
@@ -9,6 +9,16 @@
 struct Path;
 
 struct Path {
+    Path (std::vector<PathletEntry> _entries, int16_t _loopbackIndex = -1)
+    : entries { std::move(_entries) }
+    , next0 { nullptr }
+    , next1 { nullptr }
+    , next2 { nullptr }
+    , next3 { nullptr }
+    , loopbackIndex { _loopbackIndex }
+    , startIndex { 0 }
+    {}
+
     std::vector<PathletEntry> entries;
     Path* next0;
     Path* next1;

src/parts/runLine.cpp
@@ -84,7 +84,7 @@ void part::runLine (const std::string& fileName) {
         });
     }
 
-    Path path { std::move(pathletEntries), nullptr, nullptr, nullptr, nullptr, -1, 0 };
+    Path path { std::move(pathletEntries) };
 
     int64_t stash[16];
 